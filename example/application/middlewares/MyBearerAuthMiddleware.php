1f0218fd1259343e8a0cd757363f51ef525c5306
ludwig.naegele <mail@ludwig-naegele.de>
2024-05-13 00:09:52 +0200

    Update example files

---
+++ example/application/middlewares/MyBearerAuthMiddleware.php
@@ -0,0 +1,17 @@
+<?PHP
+declare(strict_types=1);
+namespace PSwag\Example\Application\Middlewares;
+
+use PSwag\Authentication\BearerAuthMiddleware;
+
+class MyBearerAuthMiddleware extends BearerAuthMiddleware
+{
+    public function getBearerFormat(): ?string {
+        return null; // "JWT"
+    }
+    
+    public function isBearerTokenValid(string $bearerToken): bool {
+        return $bearerToken == "1234";
+    }
+}
+?>
--- example/index.php
+++ example/index.php
@@ -3,8 +3,9 @@ chdir(__DIR__);
 require_once "vendor\\autoload.php";
 
 use DI\Container;
-use PSwag\Example\Application\Middlewares\ApiKeyAuthMiddleware;
-use PSwag\Example\Application\Middlewares\BasicAuthMiddleware;
+use PSwag\Example\Application\Middlewares\MyApiKeyAuthMiddleware;
+use PSwag\Example\Application\Middlewares\MyBasicAuthMiddleware;
+use PSwag\Example\Application\Middlewares\MyBearerAuthMiddleware;
 use PSwag\PSwagApp;
 use PSwag\Example\Application\Services\PetApplicationService;
 use Slim\Factory\AppFactory;
@@ -24,14 +25,15 @@ $app->setBasePath(substr($_SERVER['SCRIPT_NAME'], 0, strlen($_SERVER['SCRIPT_NAM
 // add routing middleware first, otherwise it would try to resolve route before swagger middleware can react
 $app->addRoutingMiddleware();
 
-// add swagger middleware: specify url pattern under which swagger UI shall be accessbile, and provide relative path to swagger ui dist.
-$app->addSwaggerUiMiddleware('/', 'PSwag example', '1.0.0', 'vendor/swagger-api/swagger-ui/dist/');
+// add swagger middleware: specify url pattern under which swagger UI shall be accessibile, and provide relative path to swagger ui dist.
+$app->addSwaggerUiMiddleware('/swagger', 'PSwag example', '1.0.0', 'vendor/swagger-api/swagger-ui/dist/');
 
 // register endpoints by specifying class and method name
-$app->get('/pet/{petId}', [PetApplicationService::class, 'getPetById'])->addMiddleware(new ApiKeyAuthMiddleware());
-$app->delete('/pet/{petId}', [PetApplicationService::class, 'deletePetById'])->addMiddleware(new BasicAuthMiddleware());
-$app->post('/pet', [PetApplicationService::class, 'createNewPet']);
-$app->put('/pet', [PetApplicationService::class, 'updatePetById']);
+$app->get('/pet/{petId}', [PetApplicationService::class, 'getPetById']);
+$app->delete('/pet/{petId}', [PetApplicationService::class, 'deletePetById'])->addMiddleware(new MyBasicAuthMiddleware());
+$app->post('/pet', [PetApplicationService::class, 'createNewPet'])->addMiddleware(new MyBearerAuthMiddleware());
+$app->put('/pet', [PetApplicationService::class, 'updatePetById'])->addMiddleware(new MyApiKeyAuthMiddleware());
+$app->redirect('', './index.php/swagger'); // redirect root to swagger UI
 
 $app->run();
 ?>
